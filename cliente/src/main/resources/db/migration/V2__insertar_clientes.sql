c853b4f433cb0650ac668f3bd2947d891acf17db
Fernand0SC <fe.serranoc@duocuc.cl>
2026-06-05 00:55:31 -0400

    insercion de datos para la tabla clientes

---
+++ cliente/src/main/resources/db/migration/V2__insertar_clientes.sql
@@ -0,0 +1,9 @@
+-- Insertar clientes iniciales para pruebas
+INSERT INTO clientes (nombre, apellido, email, rut)
+VALUES ('Juan', 'Perez', 'juan.perez@email.com', '12345678-9');
+
+INSERT INTO clientes (nombre, apellido, email, rut)
+VALUES ('Carlos', 'Pinto', 'carlosPintoReal@email.com', '98765432-1');
+
+INSERT INTO clientes (nombre, apellido, email, rut)
+VALUES ('Manuel', 'Gaete', 'manuel.gaete@email.com', '15975346-K');
