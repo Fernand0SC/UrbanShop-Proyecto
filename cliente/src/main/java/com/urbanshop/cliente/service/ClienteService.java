60551b1ac399f1659fccdc0533ac6b72a372b80c
Fernand0SC <fe.serranoc@duocuc.cl>
2026-06-05 00:31:24 -0400

    implementar ClienteService con metodos

--- cliente/src/main/java/com/urbanshop/cliente/service/ClienteService.java
+++ cliente/src/main/java/com/urbanshop/cliente/service/ClienteService.java
@@ -1,4 +1,34 @@
 package com.urbanshop.cliente.service;
 
+import com.urbanshop.cliente.model.Cliente;
+import com.urbanshop.cliente.repository.ClienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> findById(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void delete(Long id) {
+        clienteRepository.deleteById(id);
+    }
 }
