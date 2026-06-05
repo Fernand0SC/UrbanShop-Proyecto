6f2cad11d056c1bfc3acaa5b6a2c1de05581b679
Fernand0SC <fe.serranoc@duocuc.cl>
2026-06-05 00:21:14 -0400

    crear entidad Cliente con sus respectivos atributos y validaciones Bean Validation

--- cliente/src/main/java/com/urbanshop/cliente/model/Cliente.java
+++ cliente/src/main/java/com/urbanshop/cliente/model/Cliente.java
@@ -1,4 +1,33 @@
 package com.urbanshop.cliente.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "clientes")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede estar vacío")
+    private String apellido;
+
+    @Email(message = "El formato del email es incorrecto")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "El RUT es obligatorio")
+    @Column(unique = true)
+    private String rut;
 }
